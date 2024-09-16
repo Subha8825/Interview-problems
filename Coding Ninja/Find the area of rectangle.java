7a0438c9fd77ab48d7a947e7a6e507c43587cfa6
Subha8825 <146204172+Subha8825@users.noreply.github.com>
2024-09-16 14:58:13 +0530

    Add files via upload

---
+++ Coding Ninja/Find the area of rectangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		// Write code here
+        Scanner obj=new Scanner(System.in);
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        int c=a*b;
+        System.out.print(c);
+		
+	}
+}
+
+// Sample Input 1:
+// 4 20
+// Sample Output 1:
+// 80
+
+//Link:https://www.naukri.com/code360/problems/find-area-of-rectangle_1112574?interviewProblemRedirection=true&attempt_status=COMPLETED
