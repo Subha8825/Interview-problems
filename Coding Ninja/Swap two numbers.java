1989a1f6c5d48b30e7895f3fdc432ea845077081
Subha8825 <146204172+Subha8825@users.noreply.github.com>
2024-09-16 14:58:46 +0530

    Add files via upload

---
+++ Coding Ninja/Swap two numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution{
+    public static void swapNumber(int []a, int []b) {
+        // Write your code here.
+        int t=a[0];
+        a[0]=b[0];
+        b[0]=t;
+        
+    }
+}
+
+// Input: 
+// 'a' = 8, 'b' = 5
+
+// Output:
+// 5 8
+
+//Link:https://www.naukri.com/code360/problems/swap-two-numbers_1112577?interviewProblemRedirection=true&attempt_status=COMPLETED
+
+
